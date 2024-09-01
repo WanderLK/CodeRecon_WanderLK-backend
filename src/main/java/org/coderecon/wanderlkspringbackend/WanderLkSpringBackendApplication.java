0855fd5b7b3fb559fe239c83872ecba8fa363bd3
Ravindu Hasanka <rhatu2000@gmail.com>
2024-09-02 00:39:40 +0530

    Initial Commit

---
+++ src/main/java/org/coderecon/wanderlkspringbackend/WanderLkSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package org.coderecon.wanderlkspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderLkSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderLkSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/coderecon/wanderlkspringbackend/WanderLkSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coderecon.wanderlkspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderLkSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
