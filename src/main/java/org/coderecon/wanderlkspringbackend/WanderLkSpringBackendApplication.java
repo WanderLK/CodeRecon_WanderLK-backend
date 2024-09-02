5f7d9b99fd0c2d8d11af59ac754993a7dc3e3140
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-09-02 15:57:38 +0530

    feat: connected mongodb atlas

--- src/main/java/org/coderecon/wanderlkspringbackend/WanderLkSpringBackendApplication.java
+++ src/main/java/org/coderecon/wanderlkspringbackend/WanderLkSpringBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WanderLkSpringBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WanderLkSpringBackendApplication.class, args);
 	}
-
 }
