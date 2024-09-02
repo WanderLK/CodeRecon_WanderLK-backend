f38fac03a2f412cadb69733fc7cd55d2ec186233
Ravindu Hasanka <rhatu2000@gmail.com>
2024-09-02 21:33:14 +0530

    Bug fixed: the api endpoints whitelisted

--- src/main/java/org/coderecon/wanderlkspringbackend/Configuration/SecurityConfig.java
+++ src/main/java/org/coderecon/wanderlkspringbackend/Configuration/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        req->req.requestMatchers("api/lafresca/user/login/**","api/lafresca/user/register/**", "api/lafresca/user/refresh_token/**", "swagger-ui/**", "api-docs/**" , "api/lafresca/user/logout/**")
+                        req->req.requestMatchers("api/user/login/**","api/user/register/**", "api/user/refresh_token/**", "swagger-ui/**", "api-docs/**" , "api/user/logout/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
