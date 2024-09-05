8643dabc05f2ea8fb0dfd8e986aff3453af2ea51
aneaser <anudaattanayake@gmail.com>
2024-09-05 10:14:48 +0530

    email controlelrs added

--- src/main/java/org/coderecon/wanderlkspringbackend/Configuration/SecurityConfig.java
+++ src/main/java/org/coderecon/wanderlkspringbackend/Configuration/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        req->req.requestMatchers("api/user/login/**","api/user/register/**", "api/user/refresh_token/**", "swagger-ui/**", "api-docs/**" , "api/user/logout/**","api/visa/**", "api/country/**")
+                        req->req.requestMatchers("api/user/login/**","api/user/register/**", "api/user/refresh_token/**", "swagger-ui/**", "api-docs/**" , "api/user/logout/**","api/visa/**", "api/country/**","api/email/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
---
+++ src/main/java/org/coderecon/wanderlkspringbackend/controllers/EmailApiController.java
@@ -0,0 +1,65 @@
+package org.coderecon.wanderlkspringbackend.controllers;
+
+import org.coderecon.wanderlkspringbackend.models.Email;
+import org.coderecon.wanderlkspringbackend.services.EmailApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/email")
+public class EmailApiController {
+
+    private final WebClient webClient;
+
+    @Autowired
+    public EmailApiController(EmailApiService externalApiService, WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:4040").build();
+    }
+
+    @PostMapping("/otp")
+    public ResponseEntity<Object> otp(@RequestBody Email body) {
+        Mono<String> response = webClient.post()
+                .uri("mail/otp")
+                .bodyValue(body)  // Pass the Email object as the request body
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return ResponseEntity.ok(response.block());
+    }
+
+    @PostMapping("/approve")
+    public ResponseEntity<Object> approved(@RequestBody Email body) {
+        Mono<String> response = webClient.post()
+                .uri("mail/approved")
+                .bodyValue(body)  // Pass the Email object as the request body
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return ResponseEntity.ok(response.block());
+    }
+
+    @PostMapping("/denied")
+    public ResponseEntity<Object> denied(@RequestBody Email body) {
+        Mono<String> response = webClient.post()
+                .uri("mail/otp")
+                .bodyValue(body)  // Pass the Email object as the request body
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return ResponseEntity.ok(response.block());
+    }
+
+    @PostMapping("/completed")
+    public ResponseEntity<Object> completed(@RequestBody Email body) {
+        Mono<String> response = webClient.post()
+                .uri("mail/completed")
+                .bodyValue(body)  // Pass the Email object as the request body
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return ResponseEntity.ok(response.block());
+    }
+}
