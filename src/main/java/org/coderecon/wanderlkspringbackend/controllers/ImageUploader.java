47bd8cf7f0c4921deed858af6aa1a982237d6229
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2024-09-06 16:30:37 +0530

    Create Image Uploader controller

---
+++ src/main/java/org/coderecon/wanderlkspringbackend/controllers/ImageUploader.java
@@ -0,0 +1,10 @@
+package org.coderecon.wanderlkspringbackend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/image/")
+public class ImageUploader {
+
+}
